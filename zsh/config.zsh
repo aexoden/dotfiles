93aca3014f23a558cedec3aa18f5e4ffab74ed5e
Jason Lynch <jason@calindora.com>
2016-03-25 09:08:18 -0500

    Remove everything that initially looked unneeded.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -23,13 +23,12 @@ setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 
 setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
-setopt complete_aliases
+setopt COMPLETE_ALIASES
 
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
