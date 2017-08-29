07a69909d673f4e234e764133faab2e41faba4e7
Jason Lynch <jason@calindora.com>
2017-08-29 12:21:37 -0500

    Remove functions and slightly modify zsh config.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- topics/zsh/config.zsh
+++ topics/zsh/config.zsh
@@ -1,32 +1,23 @@
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
-
-fpath=($ZSH/functions $fpath)
-
-autoload -U $ZSH/functions/*(:t)
-
 HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=32768
+SAVEHIST=32768
 
-setopt NO_BG_NICE # don't nice background tasks
+setopt NO_BG_NICE
 setopt NO_HUP
 setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
+setopt LOCAL_OPTIONS
+setopt LOCAL_TRAPS
 setopt HIST_VERIFY
 setopt INC_APPEND_HISTORY_TIME
-setopt EXTENDED_HISTORY # add timestamps to history
+setopt EXTENDED_HISTORY
 setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_REDUCE_BLANKS
 
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
 setopt COMPLETE_ALIASES
 
 bindkey '^[[H' beginning-of-line
