7779be301a5af1d23067b44df288b6bb587876eb
Jason Lynch <jason@aexoden.com>
2023-04-19 08:32:32 -0700

    Configure less to colorize output.
    This is dependent on having lesspipe installed.
    
    Signed-off-by: Jason Lynch <jason@aexoden.com>

--- topics/zsh/colors.zsh
+++ topics/zsh/colors.zsh
@@ -4,3 +4,8 @@ alias diff='diff --color=auto'
 alias grep='grep --color=auto'
 alias ip='ip --color=auto'
 alias ls='ls --color=auto'
+
+export LESS='-R -M --shift 5'
+export LESSOPEN='|lesspipe %s'
+export LESSCOLOR=always
+export LESSCOLORIZER='pygmentize -g'
