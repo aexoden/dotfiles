2cc189ec8cc0036d1e968ad32b6cd2e70b221039
Jason Lynch <jason@aexoden.com>
2023-11-13 11:41:33 -0800

    Add some additional support for bat.
    Signed-off-by: Jason Lynch <jason@aexoden.com>

--- topics/zsh/colors.zsh
+++ topics/zsh/colors.zsh
@@ -8,3 +8,15 @@ alias ls='ls --color=auto'
 export LESS='-R -M --shift 5'
 export LESSOPEN='|lesspipe.sh %s'
 export LESSCOLOR=always
+
+if command -v bat > /dev/null ; then
+	alias cat='bat --paging=never'
+
+	batman() {
+		MANROFFOPT="-c" MANPAGER="sh -c 'col -bx | bat -l man -p'" man $@
+	}
+
+	batgitdiff() {
+		git diff --name-only --relative --diff-filter=d | xargs bat --diff
+	}
+fi
